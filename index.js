5590a78f32ede81520164578bd3f3ba72d1f220d
Kenneth Linehan <kwlinehan@kenneths-mbp-m1.myfiosgateway.com>
2024-02-21 23:20:41 -0500

    Full Stack Open Part 3a: Exercises 3.1 - 3.6: Use npm init to initialize project. Install express, nodemon and define script commands in package.json. Create index.js file and import express. Define initial persons data and configure express app. Implement node application with the following routes and methods - /api/persons GET, POST /api/persons/:id GET, DELETE and /info GET. Implement success and error response handling for routes + conditional response messaging, using switch case syntax. Implement matchingPersons function with minor refactor from phonebook in earlier exercise. Use new Date() with template string to display request date time and persons count on /info page. Set appropriate status codes for success and error responses per HTTP method. Chose next highest id Math.max + 1 in place of Math.random for id generation

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+const matchingPersons = (newName) => {
+    return persons.filter(
+        person => person.name.toLowerCase() === newName.toLowerCase()
+    )
+}
+
+app.get('/', (request, response) => {
+    response.status(200).send('<h1>phonebook</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.status(200).send(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    const match = matchingPersons(person.name).length;
+    (() => {
+        switch (true) {
+            case person.name && person.number && !match:
+                const newPerson = {...person, id: Math.max(...persons.map(person => person.id)) + 1}
+                persons = [...persons, newPerson]
+                return response.status(201).send(newPerson)
+            case !person.name || !person.number:
+                return response.status(400).send({error: 'Missing input'})
+            case match >= 1:
+                return response.status(400).send({error: 'Name must be unique'})
+        }
+    })()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id);
+    (() => {
+        switch (person ? 'found' : 'not-found') {
+            case 'found':
+                return response.json(person)
+            case 'not-found':
+                return response.status(404).end(`Cannot GET /api/persons/${id}`)
+        }
+    })()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id);
+    (() => {
+        switch (person ? 'found' : 'not-found') {
+            case 'found':
+                persons = persons.filter(person => person.id !== id)
+                return response.status(204).end()
+            case 'not-found':
+                return response.status(404).end(`Cannot DELETE /api/persons/${id}`)
+        }
+    })()
+})
+
+app.get('/info', (request, response) => {
+    response.status(200).send(`
+        <p>Phonebook has info for ${persons.length} persons</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

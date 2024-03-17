b0838db26d2a7d9b66db8abfe3e6780a6781947d
Kenneth Linehan <kwlinehan@kenneths-mbp-m1.myfiosgateway.com>
2024-03-17 15:00:00 -0400

    FSO Part 3b Exercise 3.9 - phonebook: Update PORT handling in index.js and switch to check for env variable with default of PORT 3000. Add and use express.urlencoded middleware. Test local React on :5173 against backend server running on :3000

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const logging = require('./logging')
 const express = require('express')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3000
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logging.requestDataLogger)
 
